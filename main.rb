dfe6a472e5396791f80915cf26938b24781d07dc
Harry Dhillon <navbirsdhillon@gmail.com>
2021-02-24 18:42:44 -0700

    menu added

---
+++ main.rb
@@ -0,0 +1,24 @@
+def puts_gitinfo(cmd)
+    puts `git #{cmd} -h`
+    menu
+end
+
+
+def menu
+
+    puts "1. Enter git command"
+    puts "2. Exit"
+    choice = gets.strip.to_i
+    case choice
+    when 1
+        puts "Enter git command"
+        puts_gitinfo(gets.strip)
+
+    when 2
+        Exit
+    else
+        puts "Invalid, try something else"
+        menu
+    end
+end
+menu
