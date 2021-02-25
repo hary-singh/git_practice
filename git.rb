d5129d2b2e61e272ff3931f8e367917e66bd0d30
Harry Dhillon <navbirsdhillon@gmail.com>
2021-02-24 19:45:44 -0700

    classes added

---
+++ git.rb
@@ -0,0 +1,7 @@
+module Git 
+    def self.puts_git_info(cmd)
+        puts 'Your command:'
+        puts `git #{cmd} -h`
+      end
+    
+end
--- main.rb
+++ main.rb
@@ -1,26 +1,23 @@
 require 'colorize'
-def puts_gitinfo(cmd)
-    puts `git #{cmd} -h`
-    menu
-end
-
-
-def menu
-    puts "Welcome!"
-    puts "Main menu".colorize(:cyan)
-    puts "1. Enter git command".colorize(:green)
-    puts "2. Exit".colorize(:red)
+require_relative 'git'
+class Main
+  include Git
+  def menu
+    puts 'Main menu'.colorize(:cyan)
+    puts '1. Enter git command'.colorize(:green)
+    puts '2. Exit'.colorize(:blue)
     choice = gets.strip.to_i
     case choice
     when 1
-        puts "Enter git command".colorize(:yellow)
-        puts_gitinfo(gets.strip)
-
+      puts 'Enter git command'.colorize(:yellow)
+      Git.puts_git_info(gets.strip)
     when 2
-        exit 
+      exit
     else
-        puts "Invalid, try something else".colorize(:red)
-        menu
+      puts 'Invalid Choice, try 1 or 2'.colorize(:error)
+      menu
     end
+  end
 end
-menu
+app = Main.new()
+app.menu
